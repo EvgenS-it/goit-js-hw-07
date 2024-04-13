e55f5b1b990dfcd5d0cab93f34e64ff8ccb4efee
EvgenS-it <evgens.it101@gmail.com>
2024-04-13 20:32:45 +0300

    task-1 done

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categories = document.querySelectorAll("ul#categories .item");
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(category => {
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryItems = category.querySelectorAll("li").length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Number of items: ${categoryItems}`);
+});
+
