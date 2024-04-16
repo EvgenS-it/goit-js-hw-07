5806c352431751f0154f4d18290337e85bbbf55f
EvgenS-it <evgens.it101@gmail.com>
2024-04-16 20:55:10 +0300

    task-6 JS done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector("#controls input");
+let amount = 0;
+
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const divBoxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  amount = Number(inputEl.value);
+  createBoxes(amount);
+});
+
+function createBoxes(amount) {
+  if (amount >= 1 && amount <= 100) {
+    inputEl.value = "";
+    for (let index = 0; index < amount; index += 1) {
+      const boxEl = document.createElement("div");
+      boxEl.style.width = `${30 + 10 * index}px`;
+      boxEl.style.height = `${30 + 10 * index}px`;
+      boxEl.style.backgroundColor = getRandomHexColor();
+      divBoxes.appendChild(boxEl);
+    }
+  }
+}
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function destroyBoxes() {
+  divBoxes.innerHTML = "";
+}
