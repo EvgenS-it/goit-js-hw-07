e1ff3421fee85fe94b4655c5b86554686262f6d8
EvgenS-it <evgens.it101@gmail.com>
2024-04-16 14:13:27 +0300

    task-5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeBtn = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const colorName = document.querySelector(".color");
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorName.textContent = randomColor;
+}
+
+changeBtn.addEventListener('click', changeBackgroundColor);
+
